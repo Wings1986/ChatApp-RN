755cc40287390c8508e3cec71680350725f0e3ca
wings1986 <wings1986@outlook.com>
2021-08-05 01:17:16 +0700

    firebase auth & store & gifted chat

---
+++ src/screens/RoomScreen.js
@@ -0,0 +1,229 @@
+import React, { useState, useContext, useEffect } from 'react';
+import {
+  GiftedChat,
+  Bubble,
+  Send,
+  SystemMessage
+} from 'react-native-gifted-chat';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
+import { IconButton } from 'react-native-paper';
+import { AuthContext } from '../navigation/AuthProvider';
+import firestore from '@react-native-firebase/firestore';
+import useStatsBar from '../utils/useStatusBar';
+
+
+export default function RoomScreen({ route }) {
+  useStatsBar('light-content');
+
+  const [text, setText] = useState('');
+  const [messages, setMessages] = useState([]);
+  const { thread } = route.params;
+  const { user } = useContext(AuthContext);
+  const currentUser = user.toJSON();
+
+  async function handleSend(messages) {
+    const text = messages[0].text;
+
+    firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .add({
+        text,
+        createdAt: new Date().getTime(),
+        user: {
+          _id: currentUser.uid,
+          email: currentUser.email
+        }
+      });
+
+    await firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .set(
+        {
+          latestMessage: {
+            text,
+            createdAt: new Date().getTime()
+          }
+        },
+        { merge: true }
+      );
+  }
+
+  useEffect(() => {
+    const messagesListener = firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(querySnapshot => {
+        const messages = querySnapshot.docs.map(doc => {
+          const firebaseData = doc.data();
+
+          const data = {
+            _id: doc.id,
+            text: '',
+            createdAt: new Date().getTime(),
+            ...firebaseData
+          };
+
+          if (!firebaseData.system) {
+            data.user = {
+              ...firebaseData.user,
+              name: firebaseData.user.email
+            };
+          }
+
+          return data;
+        });
+
+        setMessages(messages);
+      });
+
+    // Stop listening for updates whenever the component unmounts
+    return () => messagesListener();
+  }, []);
+
+
+  function renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: '#FF46ee'
+          },
+          right: {
+            backgroundColor: '#6646ee'
+          }
+        }}
+        textStyle={{
+          right: {
+            color: '#fff'
+          }
+        }}
+      />
+    );
+  }
+
+  function renderLoading() {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size='large' color='#6646ee' />
+      </View>
+    );
+  }
+
+  function renderSend(props) {
+    return (
+      <Send {...props}>
+        <View style={styles.sendingContainer}>
+          <IconButton icon='send-circle' size={32} color='#6646ee' />
+        </View>
+      </Send>
+    );
+  }
+
+  function scrollToBottomComponent() {
+    return (
+      <View style={styles.bottomComponentContainer}>
+        <IconButton icon='chevron-double-down' size={36} color='#6646ee' />
+      </View>
+    );
+  }
+
+  function renderSystemMessage(props) {
+    return (
+      <SystemMessage
+        {...props}
+        wrapperStyle={styles.systemMessageWrapper}
+        textStyle={styles.systemMessageText}
+      />
+    );
+  }
+
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={handleSend}
+      user={{ _id: currentUser.uid }}
+      placeholder='Type your message here...'
+      alwaysShowSend
+      showUserAvatar
+      scrollToBottom
+      renderBubble={renderBubble}
+      renderLoading={renderLoading}
+      renderSend={renderSend}
+      scrollToBottomComponent={scrollToBottomComponent}
+      renderSystemMessage={renderSystemMessage}
+    />
+
+    // <GiftedChat
+    //   messages={messages}
+    //   text={text}
+    //   onInputTextChanged={setText}
+    //   onSend={handleSend}
+    //   user={{
+    //     _id: 1,
+    //     name: 'Aaron',
+    //     avatar: 'https://placeimg.com/150/150/any',
+    //   }}
+    //   alignTop
+    //   alwaysShowSend
+    //   scrollToBottom
+    //   // showUserAvatar
+    //   renderAvatarOnTop
+    //   renderUsernameOnMessage
+    //   bottomOffset={26}
+    //   onPressAvatar={console.log}
+    //   renderInputToolbar={renderInputToolbar}
+    //   renderActions={renderActions}
+    //   renderComposer={renderComposer}
+    //   renderSend={renderSend}
+    //   renderAvatar={renderAvatar}
+    //   renderBubble={renderBubble}
+    //   renderSystemMessage={renderSystemMessage}
+    //   renderMessage={renderMessage}
+    //   renderMessageText={renderMessageText}
+    //   // renderMessageImage
+    //   renderCustomView={renderCustomView}
+    //   isCustomViewBottom
+    //   messagesContainerStyle={{ backgroundColor: 'indigo' }}
+    //   parsePatterns={(linkStyle) => [
+    //     {
+    //       pattern: /#(\w+)/,
+    //       style: linkStyle,
+    //       onPress: (tag) => console.log(`Pressed on hashtag: ${tag}`),
+    //     },
+    //   ]}
+    // />
+  );
+}
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  sendingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bottomComponentContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  systemMessageWrapper: {
+    backgroundColor: '#6646ee',
+    borderRadius: 4,
+    padding: 5
+  },
+  systemMessageText: {
+    fontSize: 14,
+    color: '#fff',
+    fontWeight: 'bold'
+  }
+});
